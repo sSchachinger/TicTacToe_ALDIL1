fd8be8fdaf8d6a22652d42fd6d7e4c9971406ddb
Simon Schachinger <s.schachinger@gmail.com>
2023-01-12 08:25:47 +0100

    inserted Gamefield and Node

--- TicTacToe_ALDIL1/Model/GameTree.cs
+++ TicTacToe_ALDIL1/Model/GameTree.cs
@@ -8,5 +8,36 @@ namespace TicTacToe_ALDIL1.Model
 {
     internal class GameTree
     {
+
     }
+
+    internal class Node<T>
+    {
+        public T value { get; set; }
+
+        public Node<T> next { get; set; }
+
+        public T _previous { get; set; }
+
+        public Node(T val, Node<T> nxt)
+        {
+            value = val;
+            next = nxt;
+        }
+    }
+    /// <summary>
+    /// 3x3 Gamefield with char-Array 
+    /// </summary>
+    internal class Gamefield
+    {
+        public char[,] Field { get; set; }
+
+        public Gamefield()
+        {
+            Field = new char[3, 3];
+        }
+    }
+
+
+
 }
